407e126f9af46c1bcd9b8676a6362ad14a495920
Daniel Park <Danielpark1239@gmail.com>
2022-06-10 22:38:01 -0400

    Deploy script to update frontend contract vars

---
+++ deploy/02-update-frontend.js
@@ -0,0 +1,38 @@
+const { ethers, network } = require("hardhat")
+const fs = require("fs")
+const FRONTEND_ADDRESSES_FILE = "../nextjs-smartcontract-lottery/constants/contractAddresses.json"
+const FRONTEND_ABI_FILE = "../nextjs-smartcontract-lottery/constants/abi.json"
+
+module.exports = async function () {
+    if (process.env.UPDATE_FRONTEND) {
+        console.log("updating frontend...")
+        updateContractAddresses()
+        updateAbi()
+        console.log("frontend updated!")
+    }
+}
+
+async function updateContractAddresses() {
+    const lottery = await ethers.getContract("Lottery")
+    const chainId = network.config.chainId.toString()
+    const contractAddresses = JSON.parse(fs.readFileSync(FRONTEND_ADDRESSES_FILE, "utf8"))
+    // frontend has chainId
+    if (chainId in contractAddresses) {
+        // update if contract address is different
+        if (!contractAddresses[chainId].includes(lottery.address)) {
+            contractAddresses[chainId].push(lottery.address)
+        }
+    }
+    // frontend doesn't have chainId
+    else {
+        contractAddresses[chainId] = [lottery.address]
+    }
+    fs.writeFileSync(FRONTEND_ADDRESSES_FILE, JSON.stringify(contractAddresses))
+}
+
+async function updateAbi() {
+    const lottery = await ethers.getContract("Lottery")
+    fs.writeFileSync(FRONTEND_ABI_FILE, lottery.interface.format(ethers.utils.FormatTypes.json))
+}
+
+module.exports.tags = ["all", "frontend"]
